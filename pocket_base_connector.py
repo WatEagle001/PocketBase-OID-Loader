77dce63b56d9350074c7cd3cf26f06d10f3a27bf
Andrea Veronese <93330631+WatEagle001@users.noreply.github.com>
2023-12-01 12:38:20 +0100

    feat: aggiunto user agen

--- pocket_base_connector.py
+++ pocket_base_connector.py
@@ -1,11 +1,9 @@
 """Salva i log in pocketbase."""
 
-from dataclasses import asdict
-
 from pocketbase import PocketBase
 from pocketbase.utils import ClientResponseError
 
-
+import httpx
 class PocketBaseClient:
     """Classe per l'interfacciamento di Pocketbase."""
 
@@ -20,16 +18,16 @@ class PocketBaseClient:
 
         Params:
         ------
-            :pb_email: e-email utente pocketbase, str
-            :pb_password: password utente pocketbase, str
-            :pb_url: base url dove risiede pocketbase, str
-            :verify_ssl: verifica o meno il certificato SSL, bool
+        pb_email: e-email utente pocketbase, str
+        pb_password: password utente pocketbase, str
+        pb_url: base url dove risiede pocketbase, str
+        verify_ssl: verifica o meno il certificato SSL, bool
 
         Returns
         -------
-            None
+        None
         """
-        self._pb_client = PocketBase(base_url=pb_url)
+        self._pb_client = PocketBase(base_url=pb_url, http_client=httpx.Client(headers={"User-Agent": "Python OID Loader"}))
         self._pb_email = pb_email
         self._pb_password = pb_password
         self._user_auth()
@@ -53,49 +51,20 @@ class PocketBaseClient:
 
         Params:
         ------
-            :sqs_message: messaggio SQS già preparato, SQSMessage
+        vedor: str
+        service: str
+        oid: str
 
         Returns
         -------
             None
         """
-        # prepara i campi base
-        '''
-        object_id: str = sqs_message.object_id
-        vendor: str = sqs_message.vendor
-        service: str = sqs_message.service
-        oid :str = sqs_message.oid
-
-        # converte in dizionario per l'inserimento nel DB
-        # la funzione `__dict__` non è abbastanza
-        body: dict = asdict(sqs_message.body)
-        '''
         # crea il messaggio nella collection
         pb_notification = self._pb_client.collection("oid_database").create(
             body_params={
                 "vendor": vendor,
                 "service": service,
                 "oid": oid
+                
             }
         )
-    '''
-        # creami i vari stati nella collection
-        # TODO: farlo più bello
-        status_id_list: list = []
-        for status in ["created", "processing", "notification_sent", "deleted"]:
-            is_done: bool = False
-            if status == "created":
-                is_done = True
-                # mettimi il creato già a True
-            id_status = self._pb_client.collection("sqs_status").create(
-                body_params={"message_id": message_id, "status_name": status, "is_done": is_done}
-            )
-            status_id_list.append(id_status.id)
-            self._status_ids.append({"status_name": status, "id": id_status.id})
-
-        self._pb_client.collection("sqs_notifications").update(
-            id=pb_notification.id, body_params={"status": status_id_list}
-        )
-
-        self._pb_notification_id = pb_notification.id
-            '''
